874bb1b89c64fe95baadcaa4dd13b167aa8d53e5
dricadias <89052954+dricadias@users.noreply.github.com>
2025-05-30 19:41:52 +0100

    body of tokenizer functions for now

---
+++ srcs/tokenizer/tokenizer.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenizer.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adias-do <adias-do@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/30 17:36:23 by adias-do          #+#    #+#             */
+/*   Updated: 2025/05/30 19:38:54 by adias-do         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+/* void	validate_token(char *input)
+{
+	// ver se os tokens sao validos
+}*/
+
+/* void	tokenize(char *input)
+{
+	// separar strings e setar tokens
+} */
+
+/* int	main(void)
+{
+	
+} */
