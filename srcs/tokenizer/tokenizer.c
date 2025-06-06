100d7749874d7abe176c2462da61f7a83b3b5983
dricadias <andrielly.12dias.ad@gmail.com>
2025-06-06 14:59:55 +0100

    trying to detect all tokens

--- srcs/tokenizer/tokenizer.c
+++ srcs/tokenizer/tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: adias-do <adias-do@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 17:36:23 by adias-do          #+#    #+#             */
-/*   Updated: 2025/06/05 20:26:21 by adias-do         ###   ########.fr       */
+/*   Updated: 2025/06/05 21:46:17 by adias-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,23 +46,33 @@ t_token	*tokenize(char *input)
 		while (input[i] == 32 || input[i] == '\t')
 			i++;
 		if (input[i] == '|')
-		{
 			new_token = create_token("|", PIPE);
-			if (!head)
-			{
-				head = new_token;
-				curr = new_token;
-			}
-			else
-			{
-				curr->next = new_token;
-				curr = new_token;
-			}
-		}
+		else if (input[i] == '>' && input[i + 1] == '>')
+			new_token = create_token(">>", APPEND);
+		else if (input[i] == '<' && input[i + 1] == '<')
+			new_token = create_token("<<", HEREDOC);
+		else if (input[i] == '<')
+			new_token = create_token("<", REDIR_IN);
+		else if (input[i] == '>')
+			new_token = create_token(">", REDIR_OUT);
 		i++;
 	}
 	return (head);
 }
+
+/*
+new_token = create_token("|", PIPE);
+if (!head)
+{
+	head = new_token;
+	curr = new_token;
+}
+else
+{
+	curr->next = new_token;
+	curr = new_token;
+}
+*/
 
 /* int	main(void)
 {
