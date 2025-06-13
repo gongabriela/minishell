fb432465feeb032f3d406e23f8583ef65745edf5
dricadias <andrielly.12dias.ad@gmail.com>
2025-06-13 03:12:44 +0100

    only made for testing tokens

--- srcs/rl_test.c
+++ srcs/rl_test.c
@@ -6,7 +6,7 @@
 /*   By: adias-do <adias-do@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 16:18:33 by adias-do          #+#    #+#             */
-/*   Updated: 2025/06/05 20:30:54 by adias-do         ###   ########.fr       */
+/*   Updated: 2025/06/13 03:02:52 by adias-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,16 @@ void	print_tokens(t_token *head)
 	{
 		if (head->type == PIPE)
 			ft_printf("PIPE -> %s\n", head->content);
+		else if (head->type == CMD)
+			ft_printf("CMD -> %s\n", head->content);
+		else if (head->type == REDIR_IN)
+			ft_printf("REDIR_IN -> %s\n", head->content);
+		else if (head->type == REDIR_OUT)
+			ft_printf("REDIR_OUT -> %s\n", head->content);
+		else if (head->type == APPEND)
+			ft_printf("APPEND -> %s\n", head->content);
+		else if (head->type == HEREDOC)
+			ft_printf("HEREDOC -> %s\n", head->content);
 		head = head->next;
 	}
 }
@@ -40,6 +50,7 @@ int	main(void)
 		}
 		tokens = tokenize(rl);
 		print_tokens(tokens);
+		//validate_token(tokens);
 		free(rl);
 	}
 	return (0);
