3704d37f14f078a83acba7ffeb7a6120a201f6f1
dricadias <andrielly.12dias.ad@gmail.com>
2025-08-31 17:14:27 +0100

    add comment

--- src/800_builtins/853_export_utils_3.c
+++ src/800_builtins/853_export_utils_3.c
@@ -6,12 +6,23 @@
 /*   By: adias-do <adias-do@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/31 17:06:41 by adias-do          #+#    #+#             */
-/*   Updated: 2025/08/31 17:06:54 by adias-do         ###   ########.fr       */
+/*   Updated: 2025/08/31 17:12:42 by adias-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
+/**
+ * @brief Removes surrounding quotes from a string if present.
+ *
+ * Checks if the string is surrounded by matching single or double quotes
+ * and removes them. If no quotes are found, returns a duplicate of the
+ * original string. Used primarily in export command processing to handle
+ * quoted variable values.
+ *
+ * @param str String to process for quote removal.
+ * @return New string with quotes removed, or duplicate if no quotes found.
+ */
 char	*remove_quotes(char *str)
 {
 	int	len;
